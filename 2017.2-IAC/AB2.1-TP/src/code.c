9312e87dd004ac4cf66e24f27eb9d9701958038a
Matheus <matas@ThiccPad>
2018-05-20 19:41:21 -0300

    functional_code

--- 2017.2-IAC/AB2.1-TP/src/code.c
+++ 2017.2-IAC/AB2.1-TP/src/code.c
@@ -9,12 +9,16 @@
  *        Created:  05/20/2018 04:14:15 AM
  *       Revision:  none
  *       Compiler:  gcc
+ *        License:  GNU
  *
  *         Author:  Matheus Artur Cabral Costa (MACC), macc@ic.ufal.br
+ *         Author:  Luis Alberto Pontes Cabus Setton (VIPER), lapcs@ic.ufal.br
+ *
  *   Organization:  UFAL
  *
  * =====================================================================================
  */
+
 #include    <sys/types.h>
 #include    <sys/sysinfo.h>
 #include    <unistd.h>
@@ -31,16 +35,22 @@ int pid = -1;
 void finish(int sig)
 {
     kill(pid,SIGKILL);
+    return ;
 }
 
 int main(int argc, char *argv[], char *envp[])
 {
-    int n;
-    n = getpid();
+    pid = fork();
+
+    char ucp[256], ucp_mem[256];
+    time_t begin, end, track;
+    int timer = 0;
+
+    sprintf(ucp, "%s%d%s", "ps -p ", pid, " -o pcpu | sed 1d | tr -d ' '");
+    sprintf(ucp_mem, "%s%d%s", "pmap -x ", pid, " | grep total | awk '{print $4}'");
 
     signal(SIGALRM,(void (*)(int))finish);
 
-    pid = fork();
 
     if(pid < 0)
     {
@@ -49,6 +59,55 @@ int main(int argc, char *argv[], char *envp[])
     else if(pid > 0)
     {
         alarm(10);
+
+        begin = time(NULL);
+        end = time(NULL);
+
+        for(;;)
+        {
+            if( strcmp(argv[1], "ucp") == 0 )
+            {
+                for(;;)
+                {
+                    if(end == begin + 10)
+                    {
+                        return 0;
+                    }
+
+                    if(end + 1 == track)
+                    {
+                        timer += 1;
+                        end = time(NULL);
+
+                        printf("%d Second(s), CPU running at:\n", timer);
+                        system(ucp);
+
+                    }
+
+                    track = time(NULL);
+                }
+            }
+            if( strcmp(argv[1], "ucp-mem") == 0 )
+            {
+                for(;;)
+                {
+                    if(end == begin + 10)
+                    {
+                        return 0;
+                    }
+                    if(end + 1 == track)
+                    {
+                        timer += 1;
+                        end = time(NULL);
+                        system(ucp);
+                        system(ucp_mem);
+                    }
+
+                    track = time(NULL);
+                }
+
+            }
+        }
         wait(NULL);
     }
     else
